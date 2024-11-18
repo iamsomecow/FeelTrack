cde38e8b16e5b0ef553104a87a533eab7e810d31
iamsomecow <145311683+iamsomecow@users.noreply.github.com>
2024-11-18 14:47:41 +1100

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,36 @@
+let data;
+const dataTemplate = {
+  Emotion : 'unset'
+  Reason : 'unset'
+}
+const currDate = new Date();
+const currDateFormated = (currDate.getDate().toString() + '/' + currDate.getMonth().toString()+ '/' + currDate.getFullYear().toString());
+function load() {
+  if(localStorage.getItem('data') !== null)
+  {
+    data = JSON.parse(localStorage.getItem('data'));
+    if (data.hasOwnProperty(currDateFormated) {
+      document.getElementById("todaysEmotion").innerHTML = data[currDateFormated].Emotion;
+    }
+  } else {
+    data = {};
+  }
+}
+load();
+function save() {
+  localStorage.setItem('data', data);
+  if (data.hasOwnProperty(currDateFormated) {
+      document.getElementById("todaysEmotion").innerHTML = data[currDateFormated].Emotion;
+    }
+}  
 function buttonClick(button) {
   alert(button.textContent)
+  if (data.hasOwnProperty(currDateFormated) {
+    data[currDateFormated].Emotion = button.textContent;
+    save();
+  } else {
+    data[currDateFormated] = dataTemplate;
+    data[currDateFormated].Emotion = button.textContent;
+    save();
+  }
 }
